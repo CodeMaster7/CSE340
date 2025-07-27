69f0f1e97520f662929db076c936e45cd0d6e0d8
Sam <sammybonfanti@gmail.com>
2025-07-26 18:02:48 -0700

    Add register view to account controller

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,14 +1,26 @@
 const utilities = require('../utilities/')
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
+ *  Deliver login view
+ * *************************************** */
 async function buildLogin(req, res, next) {
-  let nav = await utilities.getNav()
-  res.render("account/login", {
-    title: "Login",
-    nav,
-  })
+	let nav = await utilities.getNav()
+	res.render('account/login', {
+		title: 'Login',
+		nav
+	})
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+ *  Deliver register view
+ * *************************************** */
+async function buildRegister(req, res, next) {
+	let nav = await utilities.getNav()
+	res.render('account/register', {
+		title: 'Register',
+		nav,
+		errors: null
+	})
+}
+
+module.exports = { buildLogin, buildRegister }
