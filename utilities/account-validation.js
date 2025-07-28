4a7eee4ecd7d714bc3cd6ffd6cfb5fe3cf63f295
Sam <sammybonfanti@gmail.com>
2025-07-27 20:31:40 -0700

    Add email existence check in registration validation

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,6 @@
-const utilities = require('.')
 const { body, validationResult } = require('express-validator')
+const utilities = require('.')
+const accountModel = require('../models/account-model')
 
 const validate = {}
 
@@ -33,6 +34,19 @@ validate.registationRules = () => {
 			.normalizeEmail() // refer to validator.js docs
 			.withMessage('A valid email is required.'),
 
+		// valid email is required and cannot already exist in the database
+		body('account_email')
+			.trim()
+			.isEmail()
+			.normalizeEmail() // refer to validator.js docs
+			.withMessage('A valid email is required.')
+			.custom(async (account_email) => {
+				const emailExists = await accountModel.checkExistingEmail(account_email)
+				if (emailExists) {
+					throw new Error('Email exists. Please log in or use different email')
+				}
+			}),
+
 		// password is required and must be strong password
 		body('account_password')
 			.trim()
@@ -52,22 +66,22 @@ validate.registationRules = () => {
  * Check data and return errors or continue to registration
  * ***************************** */
 validate.checkRegData = async (req, res, next) => {
-  const { account_firstname, account_lastname, account_email } = req.body
-  let errors = []
-  errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    let nav = await utilities.getNav()
-    res.render("account/register", {
-      errors,
-      title: "Registration",
-      nav,
-      account_firstname,
-      account_lastname,
-      account_email,
-    })
-    return
-  }
-  next()
+	const { account_firstname, account_lastname, account_email } = req.body
+	let errors = []
+	errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		let nav = await utilities.getNav()
+		res.render('account/register', {
+			errors,
+			title: 'Registration',
+			nav,
+			account_firstname,
+			account_lastname,
+			account_email
+		})
+		return
+	}
+	next()
 }
 
 module.exports = validate
